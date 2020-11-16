5e46ae82dc6191d2c7e43063c33b2b8d0c2b16eb
Riaven <jogubaez@hotmail.com>
2020-11-16 01:31:18 -0300

    crud empleado completo

--- OdontologiaApp/apps/empleado/urls.py
+++ OdontologiaApp/apps/empleado/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import nuevoEmpleado
+from .views import nuevoEmpleado, listarEmpleados, modificarEmpleado, eliminarEmpleado
 
 urlpatterns = [
     path('nuevo/', nuevoEmpleado, name="nuevo_empleado"),
+    path('lista/', listarEmpleados, name="lista_empleado"),
+    path('eliminar/(?P<rut>\d+)/', eliminarEmpleado, name="elimina_empleado"),
+    path('modificar/(?P<rut>\d+)/', modificarEmpleado, name="modificar_empleado"),
 ]
+
+"""
+url(r'^$', listarProfesionales, name='profesionales'),
+    #url(r'^nuevo$', nuevoProfesional, name="nuevoprofesional"), 
+    url(r'^eliminar/(?P<id>\d+)/$', eliminarProfesional, name='eliminar_profesional'),
+    url(r'^modificar/(?P<id>\d+)/$', modificarProfesional, name='modificar_profesional'),
+"""
--- OdontologiaApp/apps/empleado/views.py
+++ OdontologiaApp/apps/empleado/views.py
@@ -1,15 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Empleado
 from .forms import EmpleadoForm
 # Create your views here.
 
-
+#Nuevo empleado
 def nuevoEmpleado(request):  
     if request.method == 'POST':
         form = EmpleadoForm(request.POST)       
         if form.is_valid():           
             form.save()                       
-        #return redirect('listar_fichas')   
+        return redirect('lista_empleado')   
     else:
         form = EmpleadoForm()
     return render(request, 'empleado/nuevoempleado.html', {'form': form})
+
+#Eliminar empleado
+def eliminarEmpleado(request, rut):
+    empleado = Empleado.objects.get(rut = rut)
+    if request.method == 'POST':
+        empleado.delete()
+        return redirect('lista_empleado')
+    return render(request, 'empleado/eliminarempleado.html', {'empleado': empleado})
+
+#Modificar un empleado
+def modificarEmpleado(request, rut):
+    empleado = Empleado.objects.get(rut = rut)
+    if request.method == 'GET':
+         form = EmpleadoForm(instance=empleado)
+    else:
+        form = EmpleadoForm(request.POST, instance=empleado)
+        if form.is_valid():
+            form.save()
+            return redirect('empleados')
+    return render(request, 'empleado/nuevoempleado.html', {'form': form})
+
+#listar empleados
+def listarEmpleados(request):
+    #Carga nuevo empleado
+    nuevoEmpleado(request)
+    #lista a todos los empleados
+    empleado = Empleado.objects.all()
+    contexto = {'empleados':empleado, 'form': EmpleadoForm()}
+    return render (request, 'empleado/listaempleado.html', contexto)
